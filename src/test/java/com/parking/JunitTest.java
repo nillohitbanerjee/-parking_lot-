21d0c381295493700b1be39ad8d1d46a549a65bf
nillohitbanerjee <nillohitbanerjee@gmail.com>
2019-12-13 02:27:12 +0530

    adding README.md

---
+++ src/test/java/com/parking/JunitTest.java
@@ -0,0 +1,14 @@
+package com.parking;
+
+import com.parking.commandCenter.CommandUtilTest;
+import com.parking.parkingmanager.ParkingManagerTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        /*CommandUtilTest.class,
+        ParkingManagerTest.class,*/
+})
+public class JunitTest {
+}
