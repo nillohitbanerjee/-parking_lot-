845bd03e98f4155457abd0647a8544173d8b8c8e
nillohitbanerjee <nillohitbanerjee@gmail.com>
2019-12-13 11:37:07 +0530

    adding README.md

--- src/test/java/com/parking/parkingmanager/UtilTest.java
+++ src/test/java/com/parking/parkingmanager/UtilTest.java
@@ -1,4 +1,22 @@
 package com.parking.parkingmanager;
 
-class UtilTest {
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+@RunWith(JUnit4.class)
+public class UtilTest {
+
+    @Test
+    public void testProvessFile() throws FileNotFoundException {
+
+        File inputXmlFile = new File(new ParkingManager().getClass().getResource("/parking_lot_file_inputs.txt").getFile());
+        System.out.println(inputXmlFile.getAbsolutePath());
+
+         assert (Util.processFile(inputXmlFile.getPath()));
+    }
 }
